f5f8c942a50157ea79186083621f05eaf4700d5a
Hitesh <Offline>
2023-01-18 01:48:04 +0530

    2 questions solved

--- code/008_arrays-sorting-subarrays/008-05_arraysQsAskedByTopMNC/Q2.cpp
+++ code/008_arrays-sorting-subarrays/008-05_arraysQsAskedByTopMNC/Q2.cpp
@@ -1,3 +1,5 @@
+// index of subarray which adds to a given number
+
 /*
 @2
 #? Problem:
@@ -18,32 +20,32 @@
 
     Output:2 4
 
-    Explaination: The sum of elements from 2nd position to 4th position is 12.
+    Explaination: The S of elements from 2nd position to 4th position is 12.
 
 # Brute Force:
 
-    Find sum of all possible subarrays. If any of the sum equates to S, output the starting
+    Find S of all possible subarrays. If any of the S equates to S, output the starting
     and ending index of the subarray.
 
     Time Complexity: O(n^2)
 
 Optimised:
 
-    > Keep 2 pointers start and end, and a variable currentSum sum from start to end.
+    > Keep 2 pointers start and end, and a variable currentS S from start to end.
 
     S = 5
 
          1 , 2 , 3 , 8
-   start^ ^end                  currentSum = 1
+   start^ ^end                  currentS = 1
 
          1 , 2 , 3 , 8
-   start^     ^end              currentSum = 3
+   start^     ^end              currentS = 3
 
          1 , 2 , 3 , 8
-   start^         ^end          currentSum = 6
+   start^         ^end          currentS = 6
 
          1 , 2 , 3 , 8
-       start^     ^end          currentSum = 5
+       start^     ^end          currentS = 5
 
         start=1, end=2
 
@@ -53,10 +55,69 @@ Optimised:
 */
 
 #include <iostream>
+void indexOfSubarray(int a[], int n, int s)
+{
+    int start = -1, end = -1;
+    int sum = 0;
+    int i = 0, j = 0;
+
+    while (j < n && sum + a[j] <= s)
+    {
+        sum += a[j];
+        j++;
+    }
+
+    if (sum == s)
+    {
+        std::cout << i + 1 << " " << j << "\n";
+        return;
+    }
+
+    while (j < n)
+    {
+        sum += a[j];
+        while (sum > s)
+        {
+            sum -= a[i];
+            i++;
+        }
+        if (sum == s)
+        {
+            start = i + 1;
+            end = j + 1;
+            break;
+        }
+        j++;
+    }
+
+    std::cout << start << " " << end << "\n";
+}
 
 int main()
 {
-    std::cout << "works" << std::endl;
+    // std::cout << "works" << std::endl;
+
+    /*
+    5 12
+    1 2 3 7 5
+
+
+    */
+
+    int n;
+    std::cin >> n;
+
+    int s;
+    std::cin >> s;
+
+    int a[n];
+
+    for (size_t i = 0; i < n; i++)
+    {
+        std::cin >> a[i];
+    }
+
+    indexOfSubarray(a, n, s);
 
     return 0;
 }
